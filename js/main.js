cc17b3569dc870ba4a92344b00cf4d84217e15f1
René Weiberlenn <rene.weiberlenn@live.de>
2024-10-09 08:15:14 +0200

    Revert "changed getMovies to accept a search query using a direct path to the…"

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 import {getMovies} from "./modules/network.js";
 
 window.onload = async () => {
-    const movies = await getMovies('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1');
+    const movies = await getMovies();
 
     console.log(movies);
 }
