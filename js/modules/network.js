5405b35c982e0d72eae59bc4aefaaac79eb43814
René Weiberlenn <rene.weiberlenn@live.de>
2024-10-09 17:30:09 +0200

    changed the error handling for the getMovies function to catch locally rather than throw an error later.

--- js/modules/network.js
+++ js/modules/network.js
@@ -11,25 +11,28 @@ export class Movie {
 }
 
 export const getMovies = async (query) => {
-    let movies = [];
+    try {
+        let movies = [];
 
-    const options = {
-        method: 'GET',
-        headers: {
-            accept: 'application/json',
-            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxMDVjMzY2OWRlOGY1NDUwODNjYzgzYTZlOTNhY2RkNSIsIm5iZiI6MTcyODM5NDQ3Ny4xNDQzNTUsInN1YiI6IjY3MDNjN2QyNTA4ZGZhN2JhMzc5NTJhYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.X7jR77C4O3Ik904DIzUWB_rM12RZAFAo92NF80_tZJc'
+        const options = {
+            method: 'GET',
+            headers: {
+                accept: 'application/json',
+                Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxMDVjMzY2OWRlOGY1NDUwODNjYzgzYTZlOTNhY2RkNSIsIm5iZiI6MTcyODM5NDQ3Ny4xNDQzNTUsInN1YiI6IjY3MDNjN2QyNTA4ZGZhN2JhMzc5NTJhYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.X7jR77C4O3Ik904DIzUWB_rM12RZAFAo92NF80_tZJc'
+            }
+        };
+
+        const response = await fetch(query, options);
+        const data = await response.json();
+
+        for (const movie of data.results) {
+            let newMovie = new Movie(movie.id, movie.title, movie.poster_path, movie.overview, movie.vote_average);
+            movies.push(newMovie);
         }
-    };
 
-    const response = await fetch(query, options);
-    if (!response.ok) throw new Error('Something went wrong');
-    const data = await response.json();
-
-    for (const movie of data.results) {
-        let newMovie = new Movie(movie.id, movie.title, movie.poster_path, movie.overview, movie.vote_average);
-        movies.push(newMovie);
+        return movies;
+    } catch (error) {
+        console.log(error);
     }
-
-    return movies;
 }
 
